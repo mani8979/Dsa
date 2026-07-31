55e8827323bdeec1e9d15843da9fc4e414135f1f
mani8979 <manikalla890@gmail.com>
2026-07-31 19:56:31 +0530

    Print all elements of an array

---
+++ Printallelements.java
@@ -0,0 +1,9 @@
+class Printallelements{
+    public static void main(String args[]){
+        int arr[]={10,20,30,40};
+        System.out.println("Print all elements of an array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
