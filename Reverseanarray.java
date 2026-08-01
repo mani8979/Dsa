b01df7716403c7f6a625e539774387255c9f5cde
mani8979 <manikalla890@gmail.com>
2026-08-01 10:11:04 +0530

    Reverse an array

--- Reverseanarray.java
+++ Reverseanarray.java
@@ -3,11 +3,11 @@ class Reverseanarray{
         int arr[] = {12,45,32,67,43};
         int temp = 0;
         int j = arr.length-1;
-        for(int i = 0;i< arr.length;i++){
-            temp = arr[j];
-            arr[i] = temp;
-            temp = 0;
-            --j;
+        for(int i = 0;i< arr.length/2;i++){
+            temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j--;
         }
         System.out.println("Reverse an array: ");
         for(int i = 0;i<arr.length;i++){
